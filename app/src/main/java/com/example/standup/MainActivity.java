4b99397f8a87d8aafd307cf7883d32dd03b9e1b0
Ashwani Kumar <akumar@monmouthcollege.edu>
2019-02-13 14:34:36 -0600

    set on checked click listener

--- app/src/main/java/com/example/standup/MainActivity.java
+++ app/src/main/java/com/example/standup/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.standup;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Toggle switch
+        ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
+
+        alarmToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String toastMessage;
+                if(isChecked){
+                    // Set the toast message for the "on" case
+                    toastMessage = getString(R.string.stand_up_on);
+                }else {
+                    // set the toast message for the "off" case
+                    toastMessage = getString(R.string.stand_up_off);
+                }
+                // show message to say the alarm is turned on or off
+                Toast.makeText(MainActivity.this,toastMessage,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
